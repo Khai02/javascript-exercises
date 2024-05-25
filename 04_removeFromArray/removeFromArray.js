0d668012954b94de9854c6f05bf32062933705de
Khai02 <92036912+Khai02@users.noreply.github.com>
2024-05-25 15:56:42 +0100

    Completed Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...arrayElements) {
+  const newArray = [];
+  array.forEach(function(element) {
+    if (!arrayElements.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
